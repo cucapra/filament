354084a31d4dace74a1dde6d97656821acb14092
Rachit Nigam <rachit.nigam12@gmail.com>
2022-06-30 14:45:55 -0400

    code for cocotb based test harness

---
+++ tests/compile/harness/utils.py
@@ -0,0 +1,138 @@
+# type: ignore
+
+import os
+import json
+import cocotb
+from cocotb.triggers import Timer, FallingEdge, RisingEdge
+from cocotb.clock import Clock
+from cocotb.binary import BinaryValue
+
+
+def validate_data(data):
+    """
+    Validate the data format provided to the file.
+    """
+    total_txns = len(list(data.values())[0])
+    assert all([len(vs) == total_txns for vs in data.values()]
+               ), "Invalid: Mismatched sizes for inputs"
+    return total_txns
+
+
+def construct_transaction_fsm(interface):
+    """
+    Generates a coroutine that models the provided interface.
+    The coroutine deals with the inputs in the form of a "transaction".
+    For each defined input in the `data` array, `data[<key>][i]` is part
+    of the `i` transaction sent to the module.
+
+    The coroutine executes a transaction fully before starting another one.
+    It is guaranteed to wait as long as the delay provided by the module
+    requires.
+
+    The interface defines how the inputs are sent to the module and how long
+    they are held up.
+    Similarly, the coroutine expects the outputs to be available during the
+    specified cycles and match the value in the data array.
+    """
+
+    assert len(interface["interfaces"]
+               ) == 1, "Unsupported: multiple interfaces"
+
+    # Construct a model of what needs to be done for one transaction
+    async def run(mod, data, log=False):
+        # New transaction should only trigger at the start of a cycle
+        await RisingEdge(mod.clk)
+
+        for txn in range(0, validate_data(data)):
+            ev = interface["interfaces"][0]
+
+            # Fully execute the module by triggering it till the number of
+            # times prescribed by the delay
+            for st in range(0, ev["delay"] + 1):
+                # Start the transaction by setting the interface signal to 1
+                if st == 0:
+                    trg = 1
+                else:
+                    trg = 0
+                mod._id(ev["name"], extended=False).setimmediatevalue(trg)
+
+                # Set input values
+                for inp in interface["inputs"]:
+                    assert inp["event"] == ev["event"], "input uses different event"
+                    if st >= inp["start"] and st < inp["end"]:
+                        v = data[inp["name"]][txn]
+                    else:
+                        v = BinaryValue('x')
+                    mod._id(inp["name"], extended=False).setimmediatevalue(v)
+
+                # Log things if needed
+                if log:
+                    mod._log.info("%s, go_T, %s", st, mod.go_T)
+                    for sig in data.keys():
+                        mod._log.info("%s, %s, %s", st, sig,
+                                      mod._id(sig, extended=False))
+
+                # Wait for the falling edge so that combinational computations
+                # propagate.
+                # NOTE(rachit): Not sure if this will actually work for combinational
+                # outputs.
+                await FallingEdge(mod.clk)
+
+                # Check that outputs have expected values
+                for out in interface["outputs"]:
+                    assert out["event"] == ev["event"], "output uses different event"
+                    name = out["name"]
+                    if st >= out["start"] and st < out["end"]:
+                        v = mod._id(name, extended=False).value
+                        expected = data[name][txn]
+                        assert v == expected, f"(txn {txn}: cycle {st}): Expected {name} == {expected}, actual: {v.integer}"
+
+                # Wait for end of cycle
+                await RisingEdge(mod.clk)
+
+    return run
+
+
+async def log_signal(mod, signal_name, times):
+    """
+    Log a signal every cycle for `times` number of cycles.
+    """
+    count = times
+    while count != 0:
+        count = count - 1
+        await RisingEdge(mod.clk)
+        val = mod._id(signal_name, extended=False).value
+        mod._log.info("%s = %s", signal_name, val)
+
+
+async def setup_design(mod):
+    """
+    Connects a clock to the given module and runs the reset signal for 5 cycles
+    """
+
+    await cocotb.start(Clock(mod.clk, 10, units='step').start())
+
+    # Reset phase
+    await RisingEdge(mod.clk)
+    mod.reset.setimmediatevalue(1)
+    await Timer(5, units='step')  # wait a bit
+    mod.reset.setimmediatevalue(0)
+    await FallingEdge(mod.clk)  # wait for falling edge/"negedge"
+
+
+async def run_design(mod):
+    interface_file = os.environ.get('INTERFACE')
+    assert interface_file, "Provide the location to interface file by setting the environment variable INTERFACE"
+    data_file = os.environ.get('DATA_FILE')
+    assert data_file, "Provide the location to data file by setting the environment variable DATA_FILE"
+
+    with open(interface_file) as f:
+        interface = json.load(f)
+
+    with open(data_file) as f:
+        data = json.load(f)
+
+    # Setup the design and run it
+    await setup_design(mod)
+    runner = construct_transaction_fsm(interface)
+    await runner(mod, data)
