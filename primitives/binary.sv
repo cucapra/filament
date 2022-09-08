821e69c5d2d6a31f144d496e50f24e697c4f90ae
Rachit Nigam <rachit.nigam12@gmail.com>
2022-09-08 13:07:35 +0530

    moar primitives

--- primitives/binary.sv
+++ primitives/binary.sv
@@ -1,45 +1,154 @@
 `default_nettype none
 
-module Add (
+module Const #(
+  parameter WIDTH = 32,
+  parameter VALUE = 0
+) (
+  input logic _go,
+  output [WIDTH-1:0] out
+);
+  assign out = VALUE;
+endmodule
+
+module Add #(
+  parameter WIDTH = 32
+) (
   input wire logic _go, // Unused port, only used for modeling
-  input wire logic [31:0] left,
-  input wire logic [31:0] right,
-  output wire logic [31:0] out
+  input wire logic [WIDTH-1:0] left,
+  input wire logic [WIDTH-1:0] right,
+  output wire logic [WIDTH-1:0] out
 );
   assign out = left + right;
 endmodule
 
-module MultComb (
+module Sub #(
+  parameter WIDTH = 32
+) (
   input wire logic _go, // Unused port, only used for modeling
-  input wire logic [31:0] left,
-  input wire logic [31:0] right,
-  output wire logic [31:0] out
+  input wire logic [WIDTH-1:0] left,
+  input wire logic [WIDTH-1:0] right,
+  output wire logic [WIDTH-1:0] out
+);
+  assign out = left - right;
+endmodule
+
+module MultComb #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic [WIDTH-1:0] left,
+  input wire logic [WIDTH-1:0] right,
+  output wire logic [WIDTH-1:0] out
 );
   assign out = left * right;
 endmodule
 
-module And (
+module And #(
+  parameter WIDTH = 32
+) (
   input wire logic _go, // Unused port, only used for modeling
-  input wire logic left,
-  input wire logic right,
-  output wire logic out
+  input wire logic [WIDTH-1:0] left,
+  input wire logic [WIDTH-1:0] right,
+  output wire logic [WIDTH-1:0] out
 );
   assign out = left & right;
 endmodule
 
-module Or (
+module Or #(
+  parameter WIDTH = 32
+) (
   input wire logic _go, // Unused port, only used for modeling
-  input wire logic left,
-  input wire logic right,
-  output wire logic out
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic [WIDTH-1:0] out
 );
   assign out = left | right;
 endmodule
 
-module Not (
+module Not #(
+  parameter WIDTH = 32
+) (
   input wire logic _go, // Unused port, only used for modeling
-  input wire logic in,
-  output wire logic out
+  input wire logic  [WIDTH-1:0] in,
+  output wire logic [WIDTH-1:0] out
 );
   assign out = ~in;
+endmodule
+
+module Gt #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic out
+);
+  assign out = left > right;
+endmodule
+
+module Eq #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic out
+);
+  assign out = left == right;
+endmodule
+
+module Lt #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic out
+);
+  assign out = left < right;
+endmodule
+
+module Gte #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic out
+);
+  assign out = left >= right;
+endmodule
+
+module Lte #(
+  parameter WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic  [WIDTH-1:0] left,
+  input wire logic  [WIDTH-1:0] right,
+  output wire logic out
+);
+  assign out = left <= right;
+endmodule
+
+module ZeroExtend #(
+  parameter IN_WIDTH = 32,
+  parameter OUT_WIDTH = 32
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic [IN_WIDTH-1:0] in,
+  output wire logic [OUT_WIDTH-1:0] out
+);
+  parameter EXTEND = OUT_WIDTH - IN_WIDTH;
+  assign out = {{EXTEND{1'b0}}, in};
+endmodule
+
+module Select #(
+  parameter WIDTH = 32,
+  parameter POS = 0
+) (
+  input wire logic _go, // Unused port, only used for modeling
+  input wire logic [WIDTH-1:0] in,
+  output wire logic out
+);
+  assign out = in[POS];
 endmodule
