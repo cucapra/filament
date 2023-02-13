62efbc89fe53d6c638a508734fd7b3cb076b9df4
Rachit Nigam <rachit.nigam12@gmail.com>
2023-02-12 21:59:00 -0800

    Add test

--- primitives/state.sv
+++ primitives/state.sv
@@ -38,6 +38,43 @@ Register #(WIDTH) r (
 );
 endmodule
 
+// A shift register that connects N delay elements in series.
+module Shift #(
+    parameter WIDTH = 32,
+    parameter N = 1
+) (
+  input wire clk,
+  input wire reset,
+  input wire logic [WIDTH-1:0] in,
+  output logic [WIDTH-1:0] out
+);
+
+if (N == 0) begin:pass_through
+  wire unused = clk;
+  assign out = in;
+end:pass_through
+
+else begin:chained
+  logic [N:0][WIDTH-1:0] data_delayed;
+
+  assign data_delayed[0] = in;
+  assign out = data_delayed[N];
+
+  genvar i;
+  for(i=1; i<=N; i++) begin
+      Delay #(WIDTH)
+        ch_reg (
+          .clk(clk),
+          .reset(reset),
+          .in(data_delayed[i-1]),
+          .out(data_delayed[i])
+        );
+  end
+end:chained
+
+endmodule
+
+
 // Similar to a register but provides access to its previous value only.
 // Resetting behavior controlled using the SAFE parameter.
 module Prev #(
