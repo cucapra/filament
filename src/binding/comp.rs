67d1330d600b5b938f42502cc9c622a5a9c4710e
Rachit Nigam <rachit.nigam12@gmail.com>
2023-02-19 15:31:19 -0800

    bound component responsible for building

--- src/binding/comp.rs
+++ src/binding/comp.rs
@@ -8,13 +8,6 @@ use crate::{
 };
 use std::collections::HashMap;
 
-/// Track binding information for a component
-pub struct CompBinding<'p> {
-    /// Context associated with the program
-    pub prog: &'p ProgBinding<'p>,
-    comp: BoundComponent,
-}
-
 pub(super) struct BoundComponent {
     /// Signature associated with this component
     sig: SigIdx,
@@ -26,6 +19,220 @@ pub(super) struct BoundComponent {
     inst_map: HashMap<Id, InstIdx>,
     /// Mapping from name of invocation to its index
     inv_map: HashMap<Id, InvIdx>,
+}
+
+impl BoundComponent {
+    /// Add a new instance to this binding.
+    /// Returns None when the component is not bound.
+    pub fn add_instance(
+        &mut self,
+        prog: &ProgBinding,
+        inst: &core::Instance,
+    ) -> InstIdx {
+        let sig = prog.get_sig_idx(&inst.component);
+        self.add_bound_instance(
+            inst.name.clone(),
+            sig,
+            inst.bindings.clone(),
+            inst.copy_span(),
+        )
+    }
+
+    fn add_bound_instance(
+        &mut self,
+        name: Id,
+        sig: SigIdx,
+        params: Vec<core::Expr>,
+        pos: GPosIdx,
+    ) -> InstIdx {
+        let idx = InstIdx::new(self.instances.len());
+        self.instances.push(BoundInstance::new(sig, params, pos));
+        self.inst_map.insert(name, idx);
+        idx
+    }
+
+    /// Add a new invocation to this binding. Fully resolves the binding
+    /// by filling in the default arguments.
+    /// Returns `None` when the provided instance is not bound.
+    pub fn add_invoke(
+        &mut self,
+        prog: &ProgBinding,
+        inv: &core::Invoke,
+    ) -> InvIdx {
+        let inst_idx = self.inst_map[&inv.instance];
+        let instance = &self[inst_idx];
+        let events = prog[instance.sig]
+            .event_binding(&inv.abstract_vars)
+            .into_iter()
+            .map(|b| b.1)
+            .collect();
+        self.add_bound_invoke(
+            inv.name.clone(),
+            inst_idx,
+            events,
+            inv.copy_span(),
+        )
+    }
+
+    fn add_bound_invoke(
+        &mut self,
+        name: Id,
+        instance: InstIdx,
+        events: Vec<Time>,
+        pos: GPosIdx,
+    ) -> InvIdx {
+        let idx = InvIdx::new(self.invocations.len());
+        self.invocations
+            .push(BoundInvoke::new(instance, events, pos));
+        self.inv_map.insert(name, idx);
+        idx
+    }
+
+    fn process_cmds(&mut self, prog: &ProgBinding, cmds: &Vec<core::Command>) {
+        for cmd in cmds {
+            match cmd {
+                core::Command::Instance(inst) => {
+                    self.add_instance(prog, inst);
+                }
+                core::Command::Invoke(inv) => {
+                    self.add_invoke(prog, inv);
+                }
+                core::Command::ForLoop(l) => {
+                    self.process_cmds(prog, &l.body);
+                }
+                core::Command::Connect(_) | core::Command::Fsm(_) => (),
+            }
+        }
+    }
+
+    fn process_checked_cmds(
+        &mut self,
+        prog: &ProgBinding,
+        cmds: &Vec<core::Command>,
+        diag: &mut diagnostics::Diagnostics,
+    ) -> bool {
+        let mut has_errors = false;
+        for cmd in cmds {
+            match cmd {
+                core::Command::Instance(inst) => {
+                    let comp = &inst.component;
+                    if prog.find_sig_idx(comp).is_some() {
+                        self.add_instance(prog, inst);
+                    } else {
+                        has_errors = true;
+                        // If there is no component with this name, add an error and use a dummy signature
+                        let err = Error::undefined(comp.clone(), "component")
+                            .add_note(diag.add_info(
+                                "unknown component",
+                                comp.copy_span(),
+                            ));
+                        diag.add_error(err);
+                        self.add_bound_instance(
+                            inst.name.clone(),
+                            SigIdx::UNKNOWN,
+                            vec![],
+                            inst.copy_span(),
+                        );
+                    }
+                }
+                core::Command::Invoke(inv) => {
+                    if self.inst_map.get(&inv.instance).is_some() {
+                        // If there have been previous errors, we cannot rely on signatures being valid
+                        if has_errors {
+                            self.add_bound_invoke(
+                                inv.name.clone(),
+                                InstIdx::UNKNOWN,
+                                vec![],
+                                inv.copy_span(),
+                            );
+                        } else {
+                            self.add_invoke(prog, inv);
+                        }
+                    } else {
+                        has_errors = true;
+                        // If there is no component with this name, add an error and use a dummy signature
+                        let err =
+                            Error::undefined(inv.instance.clone(), "instance")
+                                .add_note(diag.add_info(
+                                    "unknown instance",
+                                    inv.instance.copy_span(),
+                                ));
+                        diag.add_error(err);
+                        self.add_bound_invoke(
+                            inv.name.clone(),
+                            InstIdx::UNKNOWN,
+                            vec![],
+                            inv.copy_span(),
+                        );
+                    }
+                }
+                core::Command::Connect(core::Connect { src, dst, .. }) => {
+                    let mut check_port = |port: &core::Port| {
+                        if let core::PortType::InvPort { invoke, .. } =
+                            &port.typ
+                        {
+                            if self.inv_map.get(invoke).is_none() {
+                                let err = Error::undefined(
+                                    invoke.clone(),
+                                    "invocation",
+                                )
+                                .add_note(diag.add_info(
+                                    "unknown invocation",
+                                    invoke.copy_span(),
+                                ));
+                                diag.add_error(err)
+                            }
+                        } else if let core::PortType::ThisPort(p) = &port.typ {
+                            if !prog[self.sig]
+                                .ports()
+                                .iter()
+                                .any(|pd| pd.name == p)
+                            {
+                                let err = Error::undefined(p.clone(), "port")
+                                    .add_note(diag.add_info(
+                                        "unknown port",
+                                        p.copy_span(),
+                                    ));
+                                diag.add_error(err)
+                            }
+                        }
+                    };
+                    check_port(src);
+                    check_port(dst);
+                }
+                core::Command::ForLoop(l) => {
+                    has_errors |=
+                        self.process_checked_cmds(prog, &l.body, diag);
+                }
+                core::Command::Fsm(_) => (),
+            }
+        }
+
+        has_errors
+    }
+}
+
+// Index into the component binding using InstIdx
+impl std::ops::Index<InstIdx> for BoundComponent {
+    type Output = BoundInstance;
+    fn index(&self, idx: InstIdx) -> &Self::Output {
+        &self.instances[idx.get()]
+    }
+}
+
+// Index into the component binding using InvIdx
+impl std::ops::Index<InvIdx> for BoundComponent {
+    type Output = BoundInvoke;
+    fn index(&self, idx: InvIdx) -> &Self::Output {
+        &self.invocations[idx.get()]
+    }
+}
+
+/// Track binding information for a component
+pub struct CompBinding<'p> {
+    /// Context associated with the program
+    pub prog: &'p ProgBinding<'p>,
+    comp: BoundComponent,
 }
 
 impl<'p> CompBinding<'p> {
@@ -51,18 +258,15 @@ impl<'p> CompBinding<'p> {
         cmds: &Vec<core::Command>,
     ) -> Self {
         let sig = prog.get_sig_idx(comp);
-        let mut ctx = Self {
-            prog,
-            comp: BoundComponent {
-                sig,
-                instances: Vec::new(),
-                invocations: Vec::new(),
-                inst_map: HashMap::new(),
-                inv_map: HashMap::new(),
-            },
+        let mut comp = BoundComponent {
+            sig,
+            instances: Vec::new(),
+            invocations: Vec::new(),
+            inst_map: HashMap::new(),
+            inv_map: HashMap::new(),
         };
-        process_cmds(cmds, &mut ctx);
-        ctx
+        comp.process_cmds(prog, cmds);
+        Self { prog, comp }
     }
 
     /// Similar to [[Self::from_component]], does not assume that bindings are valid.
@@ -74,22 +278,18 @@ impl<'p> CompBinding<'p> {
         diag: &mut diagnostics::Diagnostics,
     ) -> Option<Self> {
         let sig = prog.get_sig_idx(comp); // Cannot throw error
-        let mut ctx = Self {
-            prog,
-            comp: BoundComponent {
-                sig,
-                instances: Vec::new(),
-                invocations: Vec::new(),
-                inst_map: HashMap::new(),
-                inv_map: HashMap::new(),
-            },
+        let mut comp = BoundComponent {
+            sig,
+            instances: Vec::new(),
+            invocations: Vec::new(),
+            inst_map: HashMap::new(),
+            inv_map: HashMap::new(),
         };
-
-        let has_errors = process_checked_cmds(&mut ctx, cmds, diag);
+        let has_errors = comp.process_checked_cmds(prog, cmds, diag);
         if has_errors {
             None
         } else {
-            Some(ctx)
+            Some(Self { prog, comp })
         }
     }
 
@@ -144,67 +344,6 @@ impl<'p> CompBinding<'p> {
             .unwrap_or_else(|| panic!("Unknown invocation: {name}"))
     }
 
-    /// Add a new instance to this binding.
-    /// Returns None when the component is not bound.
-    pub fn add_instance(&mut self, inst: &core::Instance) -> InstIdx {
-        let sig = self.prog.get_sig_idx(&inst.component);
-        self.add_bound_instance(
-            inst.name.clone(),
-            sig,
-            inst.bindings.clone(),
-            inst.copy_span(),
-        )
-    }
-
-    fn add_bound_instance(
-        &mut self,
-        name: Id,
-        sig: SigIdx,
-        params: Vec<core::Expr>,
-        pos: GPosIdx,
-    ) -> InstIdx {
-        let idx = InstIdx::new(self.comp.instances.len());
-        self.comp
-            .instances
-            .push(BoundInstance::new(sig, params, pos));
-        self.comp.inst_map.insert(name, idx);
-        idx
-    }
-
-    /// Add a new invocation to this binding. Fully resolves the binding
-    /// by filling in the default arguments.
-    /// Returns `None` when the provided instance is not bound.
-    pub fn add_invoke(&mut self, inv: &core::Invoke) -> InvIdx {
-        let inst_idx = self.comp.inst_map[&inv.instance];
-        let instance = &self[inst_idx];
-        let events = self.prog[instance.sig]
-            .event_binding(&inv.abstract_vars)
-            .into_iter()
-            .map(|b| b.1)
-            .collect();
-        self.add_bound_invoke(
-            inv.name.clone(),
-            inst_idx,
-            events,
-            inv.copy_span(),
-        )
-    }
-
-    fn add_bound_invoke(
-        &mut self,
-        name: Id,
-        instance: InstIdx,
-        events: Vec<Time>,
-        pos: GPosIdx,
-    ) -> InvIdx {
-        let idx = InvIdx::new(self.comp.invocations.len());
-        self.comp
-            .invocations
-            .push(BoundInvoke::new(instance, events, pos));
-        self.comp.inv_map.insert(name, idx);
-        idx
-    }
-
     /// Returns a resolved port definition for the given port.
     /// Returns `None` if and only if the given port is a constant.
     pub fn get_resolved_port<F>(
@@ -237,7 +376,7 @@ impl<'p> CompBinding<'p> {
 impl<'p> std::ops::Index<InstIdx> for CompBinding<'p> {
     type Output = BoundInstance;
     fn index(&self, idx: InstIdx) -> &Self::Output {
-        &self.comp.instances[idx.get()]
+        &self.comp[idx]
     }
 }
 
@@ -245,123 +384,6 @@ impl<'p> std::ops::Index<InstIdx> for CompBinding<'p> {
 impl<'p> std::ops::Index<InvIdx> for CompBinding<'p> {
     type Output = BoundInvoke;
     fn index(&self, idx: InvIdx) -> &Self::Output {
-        &self.comp.invocations[idx.get()]
+        &self.comp[idx]
     }
-}
-
-fn process_cmds(cmds: &Vec<core::Command>, ctx: &mut CompBinding) {
-    for cmd in cmds {
-        match cmd {
-            core::Command::Instance(inst) => {
-                ctx.add_instance(inst);
-            }
-            core::Command::Invoke(inv) => {
-                ctx.add_invoke(inv);
-            }
-            core::Command::ForLoop(l) => {
-                process_cmds(&l.body, ctx);
-            }
-            core::Command::Connect(_) | core::Command::Fsm(_) => (),
-        }
-    }
-}
-
-fn process_checked_cmds(
-    ctx: &mut CompBinding,
-    cmds: &Vec<core::Command>,
-    diag: &mut diagnostics::Diagnostics,
-) -> bool {
-    let mut has_errors = false;
-    for cmd in cmds {
-        match cmd {
-            core::Command::Instance(inst) => {
-                let comp = &inst.component;
-                if ctx.prog.find_sig_idx(comp).is_some() {
-                    ctx.add_instance(inst);
-                } else {
-                    has_errors = true;
-                    // If there is no component with this name, add an error and use a dummy signature
-                    let err = Error::undefined(comp.clone(), "component")
-                        .add_note(
-                            diag.add_info(
-                                "unknown component",
-                                comp.copy_span(),
-                            ),
-                        );
-                    diag.add_error(err);
-                    ctx.add_bound_instance(
-                        inst.name.clone(),
-                        SigIdx::UNKNOWN,
-                        vec![],
-                        inst.copy_span(),
-                    );
-                }
-            }
-            core::Command::Invoke(inv) => {
-                if ctx.comp.inst_map.get(&inv.instance).is_some() {
-                    // If there have been previous errors, we cannot rely on signatures being valid
-                    if has_errors {
-                        ctx.add_bound_invoke(
-                            inv.name.clone(),
-                            InstIdx::UNKNOWN,
-                            vec![],
-                            inv.copy_span(),
-                        );
-                    } else {
-                        ctx.add_invoke(inv);
-                    }
-                } else {
-                    has_errors = true;
-                    // If there is no component with this name, add an error and use a dummy signature
-                    let err =
-                        Error::undefined(inv.instance.clone(), "instance")
-                            .add_note(diag.add_info(
-                                "unknown instance",
-                                inv.instance.copy_span(),
-                            ));
-                    diag.add_error(err);
-                    ctx.add_bound_invoke(
-                        inv.name.clone(),
-                        InstIdx::UNKNOWN,
-                        vec![],
-                        inv.copy_span(),
-                    );
-                }
-            }
-            core::Command::Connect(core::Connect { src, dst, .. }) => {
-                let mut check_port = |port: &core::Port| {
-                    if let core::PortType::InvPort { invoke, .. } = &port.typ {
-                        if ctx.comp.inv_map.get(invoke).is_none() {
-                            let err =
-                                Error::undefined(invoke.clone(), "invocation")
-                                    .add_note(diag.add_info(
-                                        "unknown invocation",
-                                        invoke.copy_span(),
-                                    ));
-                            diag.add_error(err)
-                        }
-                    } else if let core::PortType::ThisPort(p) = &port.typ {
-                        if !ctx.this().ports().iter().any(|pd| pd.name == p) {
-                            let err = Error::undefined(p.clone(), "port")
-                                .add_note(
-                                    diag.add_info(
-                                        "unknown port",
-                                        p.copy_span(),
-                                    ),
-                                );
-                            diag.add_error(err)
-                        }
-                    }
-                };
-                check_port(src);
-                check_port(dst);
-            }
-            core::Command::ForLoop(l) => {
-                has_errors |= process_checked_cmds(ctx, &l.body, diag);
-            }
-            core::Command::Fsm(_) => (),
-        }
-    }
-
-    has_errors
 }
