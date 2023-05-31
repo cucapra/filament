d05fcc78d7a760e73599f00690d3d0a28b20e6f2
Rachit Nigam <rachit.nigam12@gmail.com>
2023-05-31 14:12:28 -0400

    perform interval check

--- src/ir_passes/mod.rs
+++ src/ir_passes/mod.rs
@@ -3,4 +3,5 @@ mod interval_check;
 mod type_check;
 
 pub use hoist_facts::HoistFacts;
+pub use interval_check::IntervalCheck;
 pub use type_check::TypeCheck;
--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn run(opts: &cmdline::Opts) -> Result<(), u64> {
         let mut ir = ir::transform(ns);
         ir_passes::TypeCheck::do_pass(&mut ir);
         ir_passes::HoistFacts::do_pass(&mut ir);
+        ir_passes::IntervalCheck::do_pass(&mut ir);
         ir::Printer::context(&ir, &mut std::io::stdout()).unwrap();
         return Ok(());
     }
