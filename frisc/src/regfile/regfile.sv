b76b7ab57f11ac062623985d6f61233b68a1313b
gabizon103 <ehg54@cornell.edu>
2023-06-05 13:37:29 -0400

    add regfile test

--- frisc/src/regfile/regfile.sv
+++ frisc/src/regfile/regfile.sv
@@ -1,7 +1,7 @@
 module regfile (
   input wire clk,
   input wire reset,
-  input wire logic write_en,
+  input wire logic en,
   input wire logic [4:0] rin1,
   input wire logic [4:0] rin2,
   input wire logic [4:0] rd,
@@ -16,9 +16,9 @@ module regfile (
 
   always_ff @(posedge clk) begin 
     if (reset) begin 
-      for (i = 0; i < 32; i = i+1) rf[i] <= 32'd0;
-    end else if (write_en) begin
+      for (i = 0; i < 32; i = i+1) begin rf[i] <= 32'd0; end
+    end else if (en) begin
       rf[rd] <= data;
-    end else begin end
+    end
   end
 endmodule
