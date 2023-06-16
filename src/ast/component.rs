1d9caec760fa347617e156d667cc47b9e07119f1
gabizon103 <ehg54@cornell.edu>
2023-06-16 14:16:28 -0400

    format

--- src/ast/component.rs
+++ src/ast/component.rs
@@ -1,6 +1,6 @@
 use super::{Command, Fsm, Id, Invoke, Signature};
 use crate::errors::{Error, FilamentResult};
-use std::{fmt::Display};
+use std::fmt::Display;
 
 
 /// A component in Filament
@@ -78,7 +78,7 @@ pub struct Namespace {
     /// Components defined in this file
     pub components: Vec<Component>,
     /// Top-level component id
-    pub toplevel: String
+    pub toplevel: String,
 }
 
 impl Namespace {
@@ -100,9 +100,9 @@ impl Namespace {
     /// Get the index to the top-level component.
     /// Currently, this is the distinguished "main" component
     pub fn main_idx(&self) -> Option<usize> {
-        self.components
-            .iter()
-            .position(|c| c.sig.name.inner() == &Id::from(self.toplevel.clone()))
+        self.components.iter().position(|c| {
+            c.sig.name.inner() == &Id::from(self.toplevel.clone())
+        })
     }
 }
 
