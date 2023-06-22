adc55908368da361ea4876fd471104dd0d61bfcc
gabizon103 <ehg54@cornell.edu>
2023-06-22 12:52:22 -0400

    add hw and byte instrs

--- frisc/src/extras/extras.sv
+++ frisc/src/extras/extras.sv
@@ -27,13 +27,32 @@ module OneHotMux #(
   end
 endmodule
 
-module RightShifter #(
-  parameter WIDTH = 32
+// byte addressable memory
+module ByteAccess1D #(
+  parameter SIZE = 16,
+  parameter ADDR_WIDTH = 32,
 ) (
-  input wire logic signed [WIDTH-1:0] in,
-  input wire logic [4:0] shamt,
-  input wire logic isArith, // 0 for logical, 1 for arithmetic
-  output logic [WIDTH-1:0] out
+  input wire logic clk,
+  input wire logic reset,
+  input wire logic [ADDR_WIDTH-1:0] addr0,
+  input wire logic [31:0] write_data,
+  input wire logic write_en,
+  output logic [31:0] read_data
 );
-  assign out = isArith ? in >>> shamt : in >> shamt;
+
+  logic [7:0] mem [SIZE-1:0];
+  
+  // reads
+  assign read_data[7:0] = mem[addr0];
+  assign read_data[15:8] = mem[addr0+1];
+  assign read_data[23:16] = mem[addr0+2];
+  assign read_data[31:24] = mem[addr0+3];
+
+  // writes, byte by byte based on write mask
+  always_ff @(posedge clk) begin
+    if (write_en[0]) mem[addr0] <= write_data[7:0];
+    if (write_en[1]) mem[addr0+1] <= write_data[15:8];
+    if (write_en[2]) mem[addr0+2] <= write_data[23:16];
+    if (write_en[3]) mem[addr0+3] <= write_data[31:24];
+  end
 endmodule
--- primitives/memories.sv
+++ primitives/memories.sv
@@ -43,3 +43,33 @@ module StdMem1D #(
     end
   `endif
 endmodule
+
+// 32 bit words
+module ByteAccess1D #(
+  parameter DATA_WIDTH = 32,
+  parameter SIZE = 16,
+  parameter ADDR_WIDTH = 32
+) (
+  input wire logic clk,
+  input wire logic reset,
+  input wire logic [ADDR_WIDTH-1:0] addr0,
+  input wire logic [DATA_WIDTH-1:0] write_data,
+  input wire logic write_en,
+  output logic [DATA_WIDTH-1:0] read_data
+);
+
+  logic [7:0] mem [SIZE-1:0];
+
+  // writes
+  always_ff @(posedge clk) begin
+    for(int i = 0; i < DATA_WIDTH-1; i = i+8) begin
+      mem[addr0 + i/8] <= write_en ? write_data[DATA_WIDTH-1-(8*i) -: 8] : mem[addr0 + i/8];
+    end
+  end
+
+  always_comb begin
+    for(int j = 0; j < DATA_WIDTH-1; j = j+8) begin
+      read_data[DATA_WIDTH-1-(8*j) -: 8] = mem[addr0 + j/8];
+    end
+  end
+endmodule
