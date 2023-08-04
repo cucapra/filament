afc5a97b7a574b8802115567333771e1042c06b8
Rachit Nigam <rachit.nigam12@gmail.com>
2023-08-04 11:10:39 -0400

    assign check after mono

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,6 @@ fn run(opts: &cmdline::Opts) -> Result<(), u64> {
         ir_passes::HoistFacts::do_pass(opts, &mut ir)?;
         ir_passes::Simplify::do_pass(opts, &mut ir)?;
         ir_passes::Discharge::do_pass(opts, &mut ir)?;
-        ir_passes::AssignCheck::do_pass(opts, &mut ir)?;
         // Return early if we're asked to dump the interface
         if opts.check {
             return Ok(());
@@ -69,6 +68,7 @@ fn run(opts: &cmdline::Opts) -> Result<(), u64> {
             ir::Printer::context(&ir, &mut std::io::stdout()).unwrap();
             println!("====================================================");
         }
+        ir_passes::AssignCheck::do_pass(opts, &mut ir)?;
         ir_passes::BundleElim::do_pass(&mut ir);
         if opts.show_ir {
             println!("after bundle-elim");
