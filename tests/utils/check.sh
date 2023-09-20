379dacdaa47a3f53c314ea273e7774179b7f7a5c
UnsignedByte <edlam1889@gmail.com>
2023-09-20 11:51:24 -0700

    fix check.sh to not delete edge files

--- tests/utils/check.sh
+++ tests/utils/check.sh
@@ -23,5 +23,5 @@ eps=$"$dir/params/epsilon"
 
 (fud e -s cocotb.data "$data" --to cocotb-out "$dir/harness.fil" -s calyx.flags ' -d canonicalize' -q | \
   ./"$script_dir/gen_float.py" check --fields $(cat $outfields) --dt $(cat $dtype) --epsilon $(cat $eps) --width $(cat $width) && \
-  echo "No counterexamples found" && rm -f "$data") || \
-  cat "$data"
+  echo "No counterexamples found") || \
+  (cat "$data"; exit 1)
