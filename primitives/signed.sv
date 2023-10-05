77d42f570faf0a079245535000822cfe4e0e68db
UnsignedByte <edlam1889@gmail.com>
2023-10-05 11:23:43 -0700

    change signed library

--- primitives/signed.sv
+++ primitives/signed.sv
@@ -1,12 +1,3 @@
-module NegConst #(
-  parameter WIDTH = 32,
-  parameter VALUE = 0
-) (
-  output wire logic signed [WIDTH-1:0] out
-);
-  assign out = -VALUE;
-endmodule
-
 module SAdd #(
   parameter WIDTH = 32
 ) (
