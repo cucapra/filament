e694de4f3d5e4e0f96204377ef9d8ebe9e58a7bc
Edmund Lam <edlam1889@gmail.com>
2023-10-14 07:47:40 -0700

    Add discharge pass flag (#350)
    * add discharge single flag
    
    * fix comment
    
    ---------
    
    Co-authored-by: Rachit Nigam <rachit.nigam12@gmail.com>

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -90,6 +90,10 @@ pub struct Opts {
     #[argh(option, long = "solver", default = "Solver::CVC5")]
     pub solver: Solver,
 
+    /// solve assertions separately rather than all at once
+    #[argh(switch, long = "discharge-separate")]
+    pub discharge_separate: bool,
+
     /// dump interactions with the solver in the given file
     #[argh(option, long = "dump-solver-log")]
     pub solver_replay_file: Option<String>,
