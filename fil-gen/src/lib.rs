e5bea33dd0b1aec7c00e2ab535d8625f0a997372
Rachit Nigam <rachit.nigam12@gmail.com>
2023-10-24 22:54:47 -0400

    start working on a flopoco definition

--- fil-gen/src/lib.rs
+++ fil-gen/src/lib.rs
@@ -2,4 +2,4 @@ mod cmdline;
 mod tool_schema;
 
 pub use cmdline::Opts;
-pub use tool_schema::{Instance, Manifest, Module, Tool};
+pub use tool_schema::{Instance, Manifest, Module, Tool, ToolOutput};
