f479129f73e6ac48d95343238edff7122ee7aff0
ehg54 <ehg54@cornell.edu>
2023-11-14 06:47:55 +0000

    gen with xls almost works

--- primitives/xls/butterfly/butterfly-gen.sh
+++ primitives/xls/butterfly/butterfly-gen.sh
@@ -3,40 +3,50 @@
 set -euf -o pipefail
 
 # location of the script
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DIR="$(pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# path to xls download
+XLS_PATH="$1"
 
 # name of generated module
-NAME="$1"
+NAME="$2"
 
-# absolute path to DSLX file
-SRC_PATH="$2"
+# name of file, located in this folder
+FILE_NAME="$3"
 
 # output path of generated files
-DEST_PATH="$3"
+OUT="$4"
 
 # exponent width
-EXP_WIDTH="$4"
+EXP_WIDTH="$5"
 
 # mantissa width
-MANT_WIDTH="$5"
+MANT_WIDTH="$6"
 
 # name of func to compile
-FUNC_NAME="$6"
+FUNC_NAME="$7"
 
 # number of pipeline stages
-STAGES="$7"
+STAGES="$8"
 
-mkdir "$DEST_PATH"/tmp
-cd "$DEST_PATH"/tmp
-sed "s/FSIZE/$MANT_WIDTH/g" $DIR/$SRC_PATH > $DIR/$DEST_PATH/tmp/tmp.x
-sed -i "s/ESIZE/$EXP_WIDTH/g" $DIR/$DEST_PATH/tmp/tmp.x
+PREFIX="$( cd "$( dirname "$OUT" )" >/dev/null 2>&1 && pwd )"
+XLS_REPL="$PREFIX/xls.x"
+XLS_IR="$PREFIX/xls.ir"
+XLS_OPT_IR="$PREFIX/xls.opt.ir"
+XLS_V="$PREFIX/xls.sv"
+TMP="$PREFIX/tmp"
 
-cd /scratch/ehg54/xls/
-./bazel-bin/xls/dslx/ir_convert/ir_converter_main $DIR/$DEST_PATH/tmp/tmp.x > $DIR/$DEST_PATH/tmp/impl.ir
-./bazel-bin/xls/tools/opt_main --top=__tmp__"$FUNC_NAME" $DIR/$DEST_PATH/tmp/impl.ir > $DIR/$DEST_PATH/tmp/impl.opt.ir
 
-./bazel-bin/xls/tools/codegen_main $DIR/$DEST_PATH/tmp/impl.opt.ir --top __tmp__"$FUNC_NAME" --generator=pipeline --delay_model="unit" --output_verilog_path=$DIR/$DEST_PATH/impl.v \
+sed "s/FSIZE/$MANT_WIDTH/g" $DIR/$FILE_NAME > $XLS_REPL
+sed -i "s/ESIZE/$EXP_WIDTH/g" $XLS_REPL
+
+cd "$XLS_PATH"
+./bazel-bin/xls/dslx/ir_convert/ir_converter_main $XLS_REPL > $XLS_IR --emit_fail_as_assert=false
+./bazel-bin/xls/tools/opt_main --top=__xls__"$FUNC_NAME" $XLS_IR > $XLS_OPT_IR
+
+./bazel-bin/xls/tools/codegen_main $XLS_OPT_IR --top __xls__"$FUNC_NAME" --generator=pipeline --delay_model="unit" --output_verilog_path=$XLS_V \
     --module_name="$NAME"_Exp"$EXP_WIDTH"_Mant"$MANT_WIDTH" --pipeline_stages="$STAGES" --use_system_verilog
 
-rm -rf "$DIR/$DEST_PATH/tmp"
+verilator --lint-only "$XLS_V"
+
+echo "pipeline_depth = $STAGES"
--- primitives/xls/complex/xls-gen.sh
+++ primitives/xls/complex/xls-gen.sh
@@ -5,16 +5,17 @@ set -euf -o pipefail
 # location of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# path to xls download
+XLS_PATH="$1"
+
 # name of generated module
-NAME="$1"
+NAME="$2"
 
-# absolute path to DSLX file
-SRC_PATH="$2"
-
+# name of file, located in this folder
 FILE_NAME="$3"
 
 # output path of generated files
-DEST_PATH="$4"
+OUT="$4"
 
 # exponent width
 EXP_WIDTH="$5"
@@ -28,16 +29,24 @@ FUNC_NAME="$7"
 # number of pipeline stages
 STAGES="$8"
 
-mkdir "$DEST_PATH"/tmp
-cd "$DEST_PATH"/tmp
-sed "s/FRACTION_SIZE/$MANT_WIDTH/g" $SRC_PATH/$FILE_NAME > $DEST_PATH/tmp/tmp.x
-sed -i "s/EXP_SIZE/$EXP_WIDTH/g" $DEST_PATH/tmp/tmp.x
+PREFIX="$( cd "$( dirname "$OUT" )" >/dev/null 2>&1 && pwd )"
+XLS_REPL="$PREFIX/xls.x"
+XLS_IR="$PREFIX/xls.ir"
+XLS_OPT_IR="$PREFIX/xls.opt.ir"
+XLS_V="$PREFIX/xls.sv"
+TMP="$PREFIX/tmp"
 
-cd /home/xls/
-./bazel-bin/xls/dslx/ir_convert/ir_converter_main $DEST_PATH/tmp/tmp.x > $DEST_PATH/tmp/impl.ir
-./bazel-bin/xls/tools/opt_main --top=__tmp__"$FUNC_NAME" $DEST_PATH/tmp/impl.ir > $DEST_PATH/tmp/impl.opt.ir
 
-./bazel-bin/xls/tools/codegen_main $DEST_PATH/tmp/impl.opt.ir --top __tmp__"$FUNC_NAME" --generator=pipeline --delay_model="unit" --output_verilog_path=$DEST_PATH/impl.v \
+sed "s/FRACTION_SIZE/$MANT_WIDTH/g" $DIR/$FILE_NAME > $XLS_REPL
+sed -i "s/EXP_SIZE/$EXP_WIDTH/g" $XLS_REPL
+
+cd "$XLS_PATH"
+./bazel-bin/xls/dslx/ir_convert/ir_converter_main $XLS_REPL > $XLS_IR --emit_fail_as_assert=false
+./bazel-bin/xls/tools/opt_main --top=__xls__"$FUNC_NAME" $XLS_IR > $XLS_OPT_IR
+
+./bazel-bin/xls/tools/codegen_main $XLS_OPT_IR --top __xls__"$FUNC_NAME" --generator=pipeline --delay_model="unit" --output_verilog_path=$XLS_V \
     --module_name="$NAME"_Exp"$EXP_WIDTH"_Mant"$MANT_WIDTH" --pipeline_stages="$STAGES" --use_system_verilog
 
-rm -rf "$DEST_PATH/tmp"
+verilator --lint-only "$XLS_V"
+
+echo "pipeline_depth = $STAGES"
