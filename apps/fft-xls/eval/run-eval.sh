42b9c3bfd490c6a3d7d633a7e8b602389971378f
ehg54 <ehg54@cornell.edu>
2023-11-17 17:58:26 +0000

    eval scripts

--- apps/fft-xls/eval/run-eval.sh
+++ apps/fft-xls/eval/run-eval.sh
@@ -12,7 +12,7 @@ mkdir $WORKDIR/$DST/tmp
 cargo run -- $WORKDIR/$SRC > $WORKDIR/$DST/tmp/tmp.sv
 cd $WORKDIR/$DST
 fud e -vv --from synth-verilog $WORKDIR/$DST/tmp/tmp.sv --to synth-files -o report -s synth-verilog.remote 1
-fud e --from synth-files --to resource-estimate report
+fud e --from synth-files --to resource-estimate report > $DST/rpt_parsed.json
 cd $WORKDIR/$DST
 rm -rf tmp
 
