bc8e3222fdba1189f27369d672252afd412335af
gabizon103 <ehg54@cornell.edu>
2023-11-18 21:42:43 +0000

    verilog impls

--- apps/fft-xls/eval/gen-verilog.py
+++ apps/fft-xls/eval/gen-verilog.py
@@ -7,6 +7,7 @@ args = sys.argv[1:]
 dir = args[0]
 
 for subdir in os.listdir(dir):
-    print('subdir: ' + subdir)
-    butterflies = subdir.split('_')[1]
-    subprocess.run('./apps/fft-xls/eval/gen.sh apps/eval/harness_' + butterflies + '.fil ' + dir + '/' + subdir, shell=True)
+    if "fft_" in subdir:
+        print('subdir: ' + subdir)
+        butterflies = subdir.split("_")[1]
+        subprocess.run('./apps/fft-xls/eval/gen.sh apps/fft-xls/eval/harness_' + butterflies + '.fil ' + dir + '/' + subdir, shell=True)
