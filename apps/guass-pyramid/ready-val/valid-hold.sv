302afd376c3bfc1441c539e20c40f8a8d3e778a5
Rachit Nigam <rachit.nigam12@gmail.com>
2025-08-16 23:10:23 -0400

    fix blur

--- apps/guass-pyramid/ready-val/valid-hold.sv
+++ apps/guass-pyramid/ready-val/valid-hold.sv
@@ -310,7 +310,7 @@ endmodule
 module Blur#(
   parameter D0 = 8,
   parameter D1 = 8,
-  parameter N = 1
+  parameter N = 16
 ) (
   input logic clk,
   input logic reset,
@@ -329,7 +329,7 @@ module Blur#(
 // Should apply blur kernel to each pixel neighborhood and manage ready/valid protocol
 
 logic conv_ready_i, conv_ready_o, conv_valid_i, conv_valid_o;
-logic[N-1:0][7:0] conv_in, conv_out;
+logic[15:0][7:0] conv_in, conv_out;
 
 Conv2D#(.N(N)) conv2d(
   .clk, .reset,
